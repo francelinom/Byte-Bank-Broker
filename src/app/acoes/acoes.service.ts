2a770954c45ba42f9dc5d5d96e4751dc5b6c01ed
francelino <fmarcilio1989@gmail.com>
2022-03-13 23:00:58 -0300

    Utilizando o async pipe

--- src/app/acoes/acoes.service.ts
+++ src/app/acoes/acoes.service.ts
@@ -1,4 +1,4 @@
-import { Acao } from './modelo/acoes';
+import { Acao, AcoesAPI } from './modelo/acoes';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck, tap } from 'rxjs/operators';
@@ -10,7 +10,7 @@ export class AcoesService {
   constructor(private httpClient: HttpClient) {}
 
   getAcoes() {
-    return this.httpClient.get<any>('http://localhost:3000/acoes').pipe(
+    return this.httpClient.get<AcoesAPI>('http://localhost:3000/acoes').pipe(
       tap((valor) => console.log(valor)),
       pluck('payload'),
       map((acoes) =>
